a105a4fbc1167dcd524dcce01b5513145ddee82c
Glitchdev1 <83586216+Glitch-dev1@users.noreply.github.com>
2021-08-03 12:58:31 +0000

    -added procfile -debug = False -added allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://atrptest2.herokuapp.com/', 'https://ATRPtest.glitchdev1.repl.co']
 
 
 # Application definition
