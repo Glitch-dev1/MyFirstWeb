f007d79c1a5785f991fe0c6986900d9b80e11117
Glitchdev1 <83586216+Glitch-dev1@users.noreply.github.com>
2021-08-03 13:31:53 +0000

    -just debugged on urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls import url
 
 from .views import register_request, logout_req, home_page, login_request
 from products.views import *
